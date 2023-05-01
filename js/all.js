4f5b4f72e9cfedf2c57a4d29e9647edb83f0d062
dondon <domi61503@gmail.com>
2023-05-01 22:14:05 +0800

    完成第一頁跟nav-block

--- js/all.js
+++ js/all.js
@@ -1,4 +1,20 @@
 function toggle() {
-  const hideArea = document.querySelector("#hide");
-  hideArea.classList.toggle("hide");
+  const hideArea1 = document.querySelector("#hide");
+  const hideArea2 = document.querySelector("#hide-2");
+  const hideArea3 = document.querySelector("#hide-3");
+  const hideArea4 = document.querySelector("#hide-4");
+  const bar = document.querySelector(".bar");
+
+  hideArea1.classList.toggle("hide");
+  hideArea2.classList.toggle("hide");
+  hideArea3.classList.toggle("hide");
+  hideArea4.classList.toggle("hide");
+
+  if (bar.getAttribute("name") === "bar") {
+    bar.src = "images/x.svg";
+    bar.setAttribute("name", "x");
+  } else {
+    bar.src = "images/Vector.svg";
+    bar.setAttribute("name", "bar");
+  }
 }
